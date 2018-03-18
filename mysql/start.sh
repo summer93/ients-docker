88fb387b13ce707bb66015c0f737db00b9890d91
root <root@centos-linux.shared>
2018-03-18 18:29:21 -0400

    add change.

--- mysql/start.sh
+++ mysql/start.sh
@@ -1,2 +1,3 @@
 echo "---------------start mysql image-------------------"
+docker pull daocloud.io/mysql
 docker run --name ients-mysql -v /ients-docker/mysql/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD='md123456' -p 3308:3306 -d daocloud.io/mysql
